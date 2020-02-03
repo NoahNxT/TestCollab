70e7e7212512f7912fee99a1e181789c54468e9a
tine <tinesaenen@hotmail.be>
2020-02-03 14:46:05 +0100

    header.php aangevuld en styles aangemaakt

---
+++ header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>MyCustomCMS</title>
+    <link rel="stylesheet" href="/styles.css">
+</head>
+<body>
+
+<header>
+  <div class="header">Hello</div>
+</header>
