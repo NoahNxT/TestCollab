34ceb7a6213050acf34fdb3e4bfb042062df9ad9
NoahNxT <58152024+NoahNxT@users.noreply.github.com>
2020-02-03 15:19:15 +0100

    Change title

--- header.php
+++ header.php
@@ -1,13 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>Header</title>
-    <link rel="stylesheet" href="styles.css">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <title>Split code test</title>
+  <link rel="stylesheet" href="styles.css">
 </head>
+
 <body>
 
-<header>
-  <div class="header">HEADER</div>
-</header>
+  <header>
+    <div class="header">HEADER</div>
+  </header>
