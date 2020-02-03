7fe813fbf809f78f6f312e810f05134c9de9628e
tine <tinesaenen@hotmail.be>
2020-02-03 14:49:19 +0100

    styles voor footer, titel aangepast

--- header.php
+++ header.php
@@ -3,11 +3,11 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>MyCustomCMS</title>
+    <title>Header</title>
     <link rel="stylesheet" href="/styles.css">
 </head>
 <body>
 
 <header>
-  <div class="header">Hello</div>
+  <div class="header">HEADER</div>
 </header>
